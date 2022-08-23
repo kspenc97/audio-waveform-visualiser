3dcea0a7727eb7bc839556ab6cf8c981761b44b8
kspenc97 <kspenc@tutanota.com>
2022-08-23 02:24:16 -0600

    forgot to save this file

--- src/views/OptionsPanel.vue
+++ src/views/OptionsPanel.vue
@@ -1,5 +1,12 @@
 <template>
   <div class='optionsMain'>
+    <div class="optionsSub">
+        <p>Options Panel</p>
+    </div>
+    <div class="optionsSub">
+        
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,7 +18,23 @@ export default {
 <style>
     .optionsMain {
         display: flex;
+        flex-direction: column;
+        width: 70%;
+        border-radius: 1px;
+        border-color: black;
+    }
+    .optionsSub{
+        display: flex;
         flex-direction: row;
         width: 70%;
+        border-radius: 1px;
+        border-color: black;
+    }
+    .optionsSubColumn {
+        display: flex;
+        flex-direction: column;
+        width: 70%;
+        border-radius: 1px;
+        border-color: black;
     }
 </style>
